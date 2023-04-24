ef0d40ad858e9c46ffbdf77033b59f430bd02ad4
Pradeep P <pradeep23910@gmail.com>
2023-04-24 06:05:16 +0000

    major commit

---
+++ BTP/dataset.py
@@ -0,0 +1,90 @@
+import numpy as np
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from skimage import io
+
+class CheXpertData(Dataset):
+    def __init__(self, label_path, mode='train'):
+        self._label_header = None
+        self._image_paths = []
+        self._labels = []
+        self._mode = mode
+        self.transform = transforms.Compose(
+            [transforms.ToPILImage(),
+             transforms.Resize((224, 224), interpolation=transforms.InterpolationMode.NEAREST),
+             transforms.Grayscale(num_output_channels=3),
+             transforms.ToTensor(),
+             transforms.Normalize(mean=[.5], std=[.5])])
+        self.dict = [{'1.0': '1', '': '0', '0.0': '0', '-1.0': '0'},
+                     {'1.0': '1', '': '0', '0.0': '0', '-1.0': '1'}, ]
+        with open(label_path) as f:
+            header = f.readline().strip('\n').split(',')
+            self._label_header = [
+                header[7],
+                header[10],
+                header[11],
+                header[13],
+                header[15]]
+            c1 = 0 
+            c2 = 0
+            c3 = 0
+            c4 = 0
+            c5 = 0
+            for line in f:
+                labels = []
+                fields = line.strip('\n').split(',')
+                image_path = fields[0]
+                for index, value in enumerate(fields[5:]):
+                    if index == 5 or index == 8:
+                        labels.append(self.dict[1].get(value))
+                    elif index == 2 or index == 6 or index == 10:
+                        labels.append(self.dict[0].get(value))
+                    """
+                    if index == 2 and self.dict[0].get(value) == '1':
+                        c1+=1
+                    elif index == 5 and self.dict[1].get(value) == '1':
+                        c2+=1
+                    elif index == 6 and self.dict[0].get(value) == '1':
+                        c3+=1
+                    elif index == 8 and self.dict[1].get(value) == '1':
+                        c4+=1
+                    elif index == 10 and self.dict[0].get(value) == '1':
+                        c5+=1
+                    """
+                labels = list(map(int, labels))
+                self._image_paths.append(image_path)
+                self._labels.append(labels)
+                """
+                if c1<=9000 and labels[0]==1:
+                    self._image_paths.append(image_path)
+                    self._labels.append(labels)
+                elif c2<=9000 and labels[1]==1:
+                    self._image_paths.append(image_path)
+                    self._labels.append(labels)
+                elif c3<=9000 and labels[2]==1:
+                    self._image_paths.append(image_path)
+                    self._labels.append(labels)
+                elif c4<=9000 and labels[3]==1:
+                    self._image_paths.append(image_path)
+                    self._labels.append(labels)
+                elif c5<=9000 and labels[4]==1:
+                    self._image_paths.append(image_path)
+                    self._labels.append(labels)
+                """
+        self._num_image = len(self._image_paths)
+
+    def __len__(self):
+        return self._num_image
+
+    def __getitem__(self, idx):
+        image = io.imread(self._image_paths[idx])
+        image = self.transform(image)
+        labels = np.array(self._labels[idx]).astype(np.float32)
+        path = self._image_paths[idx]
+
+        if self._mode == 'train' or self._mode == 'val':
+            return (image, labels)
+        elif self._mode == 'test':
+            return (image, path)
+        else:
+            raise Exception('Unknown mode : {}'.format(self._mode))
---
+++ train.py
@@ -0,0 +1,342 @@
+import torch
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+from torch.nn.functional import one_hot
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+from captum.attr import LayerGradCam, LayerAttribution, LRP
+from tqdm import tqdm, trange
+from torchvision.models import densenet121
+import torchvision.models as models
+import torch.utils.model_zoo as model_zoo
+import datetime
+import pytz
+
+from losses import ClassDistinctivenessLoss, SpatialCoherenceConv, sigmoid_focal_loss
+from libauc.losses import AUCMLoss
+from libauc.optimizers import PESG
+from metrics import AUC
+from utils import EarlyStopping
+
+class Trainer:
+    def __init__(self, args):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.n_classes = args.n_classes
+        self.batch_size = args.batch_size
+        self.data = args.data
+        self.lr = args.lr
+        self.beta1 = args.beta1
+        self.beta2 = args.beta2
+        self.drop_rate = args.drop_rate
+        self.training_type = args.training_type
+        self.wceloss = args.wceloss
+        self.focalloss = args.focalloss
+        self.gamma = args.gamma
+        self.alpha = args.alpha
+        self.cdloss = args.cdloss
+        self.cdloss_weight = args.cdloss_weight
+        self.scloss = args.scloss
+        self.scloss_weight = args.scloss_weight
+        self.grid_search = args.grid_search
+
+        if self.training_type == 'double-stage':
+            self.model = densenet121(drop_rate=self.drop_rate)
+            self.model.classifier = nn.Linear(1024, self.n_classes)
+            self.model.load_state_dict(torch.load('model-weights/{}_wce.pt'.format(self.data)))
+        if self.training_type == 'dam':
+            # self.model = densenet121(drop_rate=self.drop_rate)
+            self.model = densenet121(weights='DEFAULT', drop_rate=self.drop_rate)
+            self.model.classifier = nn.Linear(1024, self.n_classes)
+            #   
+        elif self.training_type == 'vgg19':
+            # self.model = models.__dict__['vgg'](num_classes = 5)
+            self.model = models.vgg19(weights = 'DEFAULT')
+            # pretrained_state = model_zoo.load_url('https://download.pytorch.org/models/vgg19-dcbb9e9d.pth')
+            self.model.classifier = nn.Linear(25088, self.n_classes)
+        elif self.training_type == 'resnet101':
+            self.model = models.resnet101(weights = 'DEFAULT')
+            self.model.avgpool = nn.AdaptiveAvgPool2d(1)
+            self.model.fc = nn.Linear(2048, self.n_classes)
+            # self.model.classifier = nn.Linear(2048, self.n_classes)
+        else:
+            self.model = densenet121(weights='DEFAULT', drop_rate=self.drop_rate)
+            self.model.classifier = nn.Linear(1024, self.n_classes)
+        self.model = self.model.to(self.device)
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr, betas=(self.beta1, self.beta2))
+        self.sigmoid = nn.Sigmoid()
+
+        self.metrics = AUC()
+        self.early_stopping = EarlyStopping(args)
+
+        self.train_losses = []
+        self.val_losses = []
+        self.train_aucs = []
+        self.val_aucs = []
+
+        self.loss_df = pd.DataFrame(columns=['Training Loss','Validation Loss'])
+        if args.data=='chexpert':
+            self.train_auc_df = pd.DataFrame(columns=['Atelectasis','Cardiomegaly','Consolidation','Edema','Plural Effusion','Mean'])
+            self.val_auc_df = pd.DataFrame(columns=['Atelectasis','Cardiomegaly','Consolidation','Edema','Plural Effusion','Mean'])
+        if args.data=='breastmnist':
+            self.train_auc_df = pd.DataFrame(columns=['class1','class2','Mean'])
+            self.val_auc_df = pd.DataFrame(columns=['class1','class2','Mean'])
+        self.loss_batch_df = pd.DataFrame(columns=['custom loss','cross entropy','class distinctiveness','spatial coherence'])
+
+    def train_one_epoch(self):
+        self.model.train()
+        
+        losses = []
+        y_true = torch.tensor([]).to(self.device)
+        y_pred = torch.tensor([]).to(self.device)  
+        if self.training_type=='dam':     
+            self.wceloss=False       
+            self.damLoss = AUCMLoss()
+            """
+            self.optimizer = PESG(self.model, 
+                            a=self.damLoss.a, 
+                            b=self.damLoss.b, 
+                            alpha=self.damLoss.alpha, 
+                            imratio=None, 
+                            lr=0.1, 
+                            gamma=0.9,            
+                            epoch_decay = None                                                 
+                            )            
+            """                  
+            
+        for inputs, targets in tqdm(self.train_loader):            
+            self.optimizer.zero_grad()                    
+            inputs = inputs.to(self.device)                        
+            outputs = self.model(inputs)            
+
+            loss = 0
+            
+            if self.data == 'breastmnist':
+                targets = one_hot(targets, num_classes=2)
+            targets = torch.squeeze(targets, 1)
+
+            data_hist = np.zeros(self.n_classes)
+            for target in targets:
+                ind = np.where(target==1)
+                data_hist[ind] += 1
+            data_hist /= self.train_loader.batch_size
+            ce_weights = torch.Tensor(data_hist).to(self.device)
+            criterion = nn.BCEWithLogitsLoss(weight=ce_weights)            
+            targets = targets.float()
+            targets = targets.to(self.device)
+
+            if self.wceloss:
+                wceloss = criterion(outputs, targets)
+                loss = loss + wceloss
+                # print(f'{loss.size()}, {wceloss.size()}')
+                # print(f'{type(loss)}, {type(wceloss)}')
+            if self.training_type=='dam':
+                damLossVal = self.damLoss(outputs, targets)
+                loss = loss + damLossVal
+                # print(f'{loss.size()}, {damLossVal.size()}')
+                # print(f'{type(loss[0])}, {type(damLossVal[0])}')
+                # print(f'{loss}, {damLossVal}')
+            if self.focalloss:
+                focalloss = sigmoid_focal_loss(outputs, targets, self.alpha, self.gamma)
+                loss = loss + focalloss
+
+            if self.cdloss or self.scloss:
+                if self.training_type == 'vgg19':
+                    gradcam = LayerGradCam(self.model, layer=self.model.features[34])
+                elif self.training_type == 'resnet101':
+                    gradcam = LayerGradCam(self.model, layer=self.model.layer4[2].conv3)
+                else:
+                    gradcam = LayerGradCam(self.model, layer=self.model.features.denseblock4.denselayer16.conv2)
+                attr_classes = [torch.Tensor(gradcam.attribute(inputs=inputs, target = [i] * inputs.shape[0])).to(self.device) for i in range(self.n_classes)]
+
+                if self.cdloss:
+                    cdcriterion = ClassDistinctivenessLoss(device=self.device)
+                    cdloss_value = cdcriterion(attr_classes)
+                    cdloss_value = self.cdloss_weight * cdloss_value
+                    loss = loss + cdloss_value
+
+                if self.scloss:
+                    upsampled_attr_val = [LayerAttribution.interpolate(attr, inputs[0].shape[-2:]) for
+                                        attr in attr_classes]
+                    sccriterion = SpatialCoherenceConv(device=self.device, kernel_size=9)
+                    scloss_value = sccriterion(upsampled_attr_val, device=self.device)
+                    scloss_value = self.scloss_weight * scloss_value
+                    loss = loss + scloss_value
+                    
+            predictions = self.sigmoid(outputs).to(self.device)
+            y_pred = torch.cat((y_pred, predictions), 0)
+            y_true = torch.cat((y_true, targets), 0)
+            losses.append(loss.item())
+            
+            loss.backward()
+            self.optimizer.step()
+
+        self.train_losses.append(sum(losses) / len(losses))
+        self.train_aucs.append(self.metrics(y_pred, y_true))
+
+    def evaluate(self):
+        #self.model.eval()
+
+        losses = []
+        y_true = torch.tensor([]).to(self.device)
+        y_pred = torch.tensor([]).to(self.device)
+
+        with torch.no_grad():
+            for inputs, targets in tqdm(self.val_loader):
+                inputs = inputs.to(self.device)
+                outputs = self.model(inputs)
+
+                loss = 0 
+
+                if self.data == 'breastmnist':
+                    targets = one_hot(targets, num_classes=2)
+                targets = torch.squeeze(targets, 1)
+
+                data_hist = np.zeros(self.n_classes)
+                for target in targets:
+                    ind = np.where(target==1)
+                    data_hist[ind] += 1
+                data_hist /= self.train_loader.batch_size
+                ce_weights = torch.Tensor(data_hist).to(self.device)
+                criterion = nn.BCEWithLogitsLoss(weight=ce_weights)
+                damLoss = AUCMLoss(imratio=None)                
+                targets = targets.float()
+                targets = targets.to(self.device)
+
+                if self.wceloss:
+                    wceloss = criterion(outputs, targets)
+                    loss = loss + wceloss
+                if self.training_type=='dam':
+                    damLossVal = damLoss(outputs, targets)
+                    loss = loss + damLossVal
+
+                if self.focalloss:
+                    focalloss = sigmoid_focal_loss(outputs, targets, self.alpha, self.gamma)
+                    loss = loss + focalloss
+
+                if self.cdloss or self.scloss:
+                    if self.training_type == 'vgg19':
+                        gradcam = LayerGradCam(self.model, layer=self.model.features[34])
+                    elif self.training_type == 'resnet101':
+                        gradcam = LayerGradCam(self.model, layer=self.model.layer4[2].conv3)
+                    else:
+                        gradcam = LayerGradCam(self.model, layer=self.model.features.denseblock4.denselayer16.conv2)
+                    attr_classes = [torch.Tensor(gradcam.attribute(inputs=inputs, target = [i] * inputs.shape[0])).to(self.device) for i in range(self.n_classes)]
+
+                    if self.cdloss:
+                        cdcriterion = ClassDistinctivenessLoss(device=self.device)
+                        cdloss_value = cdcriterion(attr_classes)
+                        cdloss_value = self.cdloss_weight * cdloss_value
+                        loss = loss + cdloss_value
+
+                    if self.scloss:
+                        upsampled_attr_val = [LayerAttribution.interpolate(attr, inputs[0].shape[-2:]) for
+                                            attr in attr_classes]
+                        sccriterion = SpatialCoherenceConv(device=self.device, kernel_size=9)
+                        scloss_value = sccriterion(upsampled_attr_val, device=self.device)
+                        scloss_value = self.scloss_weight * scloss_value
+                        loss = loss + scloss_value
+                
+                predictions = self.sigmoid(outputs).to(self.device)
+
+                y_pred = torch.cat((y_pred, predictions), 0)
+                y_true = torch.cat((y_true, targets), 0)
+                losses.append(loss.item())
+
+        self.val_losses.append(sum(losses) / len(losses))
+        self.val_aucs.append(self.metrics(y_pred, y_true))
+
+    def train(self, train_loader, val_loader, epochs):
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.epochs = epochs
+        best_val_auc = 0
+
+        for epoch in trange(self.epochs):
+            self.train_one_epoch()
+            self.evaluate()
+            
+            print("Epoch {0}: Training Loss = {1}, Validation Loss = {2}, Average Training AUC = {3}, Average Validation AUC = {4}".
+            format(epoch+1, self.train_losses[-1], self.val_losses[-1], 
+            sum(self.train_aucs[-1])/self.n_classes, sum(self.val_aucs[-1])/self.n_classes))
+
+            losses = [self.train_losses[-1], self.val_losses[-1]]
+            self.loss_df.loc['Epoch {}'.format(epoch+1)] = np.array(losses)
+
+            train_auc = self.train_aucs[-1]
+            train_auc = train_auc.tolist()
+            train_auc.append(sum(train_auc)/len(train_auc))
+            self.train_auc_df.loc['Epoch {}'.format(epoch+1)] = train_auc
+
+            val_auc = self.val_aucs[-1]
+            val_auc = val_auc.tolist()
+            val_auc.append(sum(val_auc)/len(val_auc))
+            self.val_auc_df.loc['Epoch {}'.format(epoch+1)] = val_auc
+            best_val_auc = max(best_val_auc, sum(val_auc)/len(val_auc))
+
+            self.early_stopping(self.val_losses[-1], self.model, epoch)
+            """
+            if self.early_stopping.early_stop:
+                print("Early stopping")
+                break
+            """
+
+        time = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
+        time = str(time.day) + '_' + str(time.month) + '_' + str(time.year) + '_' + str(time.hour) + '_' + str(time.minute) + '_' + str(time.second)
+
+        if self.grid_search==False:
+            if self.training_type=='double-stage':
+                self.loss_df.to_csv('results/{}_loss_double_stage_{}.csv'.format(self.data, time))
+                self.train_auc_df.to_csv('results/{}_train_auc_double_stage_{}.csv'.format(self.data, time))
+                self.val_auc_df.to_csv('results/{}_val_auc_double_stage_{}.csv'.format(self.data, time))
+            elif self.training_type=='dam':
+                self.loss_df.to_csv('results/{}_loss_dam_{}.csv'.format(self.data, time))
+                self.train_auc_df.to_csv('results/{}_train_auc_dam_{}.csv'.format(self.data, time))
+                self.val_auc_df.to_csv('results/{}_val_auc_dam_{}.csv'.format(self.data, time))                
+            elif self.training_type=='vgg19':
+                if not self.cdloss:
+                    self.loss_df.to_csv('results/{}_loss_vgg19_{}.csv'.format(self.data, time))
+                    self.train_auc_df.to_csv('results/{}_train_auc_vgg19_{}.csv'.format(self.data, time))
+                    self.val_auc_df.to_csv('results/{}_val_auc_vgg19_{}.csv'.format(self.data, time))
+                else:
+                    self.loss_df.to_csv('results/{}_vgg19_loss_wce_cd_{}.csv'.format(self.data, time))
+                    self.train_auc_df.to_csv('results/{}_vgg19_train_auc_wce_cd_{}.csv'.format(self.data, time))
+                    self.val_auc_df.to_csv('results/{}_vgg19_val_auc_wce_cd_{}.csv'.format(self.data, time))                    
+            elif self.training_type=='resnet101':
+                if not self.cdloss:
+                    self.loss_df.to_csv('results/{}_loss_resnet101_{}.csv'.format(self.data, time))
+                    self.train_auc_df.to_csv('results/{}_train_auc_resnet101_{}.csv'.format(self.data, time))
+                    self.val_auc_df.to_csv('results/{}_val_auc_resnet101_{}.csv'.format(self.data, time))
+                else:
+                    self.loss_df.to_csv('results/{}_resnet101_loss_wce_cd_{}.csv'.format(self.data, time))
+                    self.train_auc_df.to_csv('results/{}_resnet101_train_auc_wce_cd_{}.csv'.format(self.data, time))
+                    self.val_auc_df.to_csv('results/{}_resnet101_val_auc_wce_cd_{}.csv'.format(self.data, time))
+            elif self.cdloss and self.scloss:
+                self.loss_df.to_csv('results/{}_loss_wce_cd_sc_{}.csv'.format(self.data, time))
+                self.train_auc_df.to_csv('results/{}_train_auc_wce_cd_sc_{}.csv'.format(self.data, time))
+                self.val_auc_df.to_csv('results/{}_val_auc_wce_cd_sc_{}.csv'.format(self.data, time))
+            elif self.cdloss:
+                self.loss_df.to_csv('results/{}_loss_wce_cd_{}.csv'.format(self.data, time))
+                self.train_auc_df.to_csv('results/{}_train_auc_wce_cd_{}.csv'.format(self.data, time))
+                self.val_auc_df.to_csv('results/{}_val_auc_wce_cd_{}.csv'.format(self.data, time))
+            elif self.scloss:
+                self.loss_df.to_csv('results/{}_loss_wce_sc_{}.csv'.format(self.data, time))
+                self.train_auc_df.to_csv('results/{}_train_auc_wce_sc_{}.csv'.format(self.data, time))
+                self.val_auc_df.to_csv('results/{}_val_auc_wce_sc_{}.csv'.format(self.data, time))
+            elif self.focalloss:
+                self.loss_df.to_csv('results/{}_loss_focal_{}.csv'.format(self.data, time))
+                self.train_auc_df.to_csv('results/{}_train_auc_focal_{}.csv'.format(self.data, time))
+                self.val_auc_df.to_csv('results/{}_val_auc_focal_{}.csv'.format(self.data, time))
+            else:
+                self.loss_df.to_csv('results/{}_loss_wce_{}.csv'.format(self.data, time))
+                self.train_auc_df.to_csv('results/{}_train_auc_wce_{}.csv'.format(self.data, time))
+                self.val_auc_df.to_csv('results/{}_val_auc_wce_{}.csv'.format(self.data, time))
+        else:
+            self.loss_batch_df.to_csv('results/{}_{}_{}.csv'.format(self.data, self.cdloss_weight, time))
+            self.train_auc_df.to_csv('results/{}_{}_train_auc_wce_{}.csv'.format(self.data, self.cdloss_weight, time))
+            self.val_auc_df.to_csv('results/{}_{}_val_auc_wce_{}.csv'.format(self.data, self.cdloss_weight, time))
+            with open("output.txt", "a") as f:
+                print("Best val accuracy with cdloss weight = {} is {}".format(self.cdloss_weight, best_val_auc), file=f)
+        
